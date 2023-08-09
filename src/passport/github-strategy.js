760ffb08f1223b390000683c34b9a50411c09f23
Ignacio Pedrosa <ignapedrosa2@gmail.com>
2023-08-09 20:39:10 -0300

    Callback url cambio

--- src/passport/github-strategy.js
+++ src/passport/github-strategy.js
@@ -4,16 +4,12 @@ import passport from 'passport';
 import UserDao from '../daos/mongodb/user.dao.js';
 const userDao = new UserDao();
 
-// const strategyOptions = {
-//     clientID: 'Iv1.3c4696932577965e',
-//     clientSecret: '24d21d0a9d0c6c15880a55cc0ba3a6be18246129',
-//     callbackURL: 'http://localhost:3000/users/profile-github',
-// };
 
 const strategyOptions = {
     clientID: 'Iv1.0bea34365e4f6b4e',
     clientSecret: '4acf33f8135f2d2d63a68d31f5949b2c07d1974c',
-    callbackURL: '/users/profile-github',
+    callbackURL: 'https://iapedrosashop.adaptable.app/users/register-github'
+
 };
 
 const registerOrLogin = async (accessToken, refreshToken, profile, done) => {
