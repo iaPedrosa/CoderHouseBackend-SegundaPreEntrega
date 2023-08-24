0660ca58bcd014b9665fb5c31393c1f2e125804f
Ignacio Pedrosa <ignapedrosa2@gmail.com>
2023-08-24 13:07:06 -0300

    .env

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 try {
   await connect(
-    process.env.MONGO_LOCAL_URL
+    process.env.MONGO_ATLAS_URL
   );
   console.log("Conectado a la base de datos de MongoDB");
 } catch (error) {
