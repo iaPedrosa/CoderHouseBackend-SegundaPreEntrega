f8e46c941118ae03f3858246ddf44c0eaf05aaf9
Ignacio Pedrosa <ignapedrosa2@gmail.com>
2023-08-24 13:24:52 -0300

    .env

--- src/db/database.js
+++ src/db/database.js
@@ -1,13 +1,12 @@
-import { connect } from "mongoose";
+import mongoose from 'mongoose';
 import 'dotenv/config';
 
-try {
-  await connect(
-    process.env.MONGO_ATLAS_URL
-  );
-  console.log("Conectado a la base de datos de MongoDB");
-} catch (error) {
-  console.log(error);
-}
+export const connectionString = process.env.DATABASE_URL;
 
-
+  try {
+    
+    await mongoose.connect(connectionString);
+    console.log('Conectado a la base de datos de MongoDB');
+  } catch (error) {
+    console.log(`ERROR => ${error}`);
+  }
